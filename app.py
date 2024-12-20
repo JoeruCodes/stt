56bd5b7c414919c30a203215585d2a1a83a02b89
JoeruCodes <joymedi8143@gmail.com>
2024-12-20 20:30:02 +0530

    added stuff

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def transcribe_audio():
     return jsonify({"transcribed_text": recognized_text}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=6942)
